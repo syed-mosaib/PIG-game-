a525bff34603232472d0eb7116d8cfe45fefa4cc
syed <syedmosaib231@gmail.com>
2024-07-11 18:25:55 +0530

    make changes

---
+++ PIG.py
@@ -0,0 +1,17 @@
+import random
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+while True:
+    players = input("Enter the number of players (1-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2<= players <=4:
+            break
+    else:
+        print("Invalid, try again.")
